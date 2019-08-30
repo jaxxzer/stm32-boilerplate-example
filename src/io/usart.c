de66b42ba14175c9ee7b166786838e4f96e61a8c
Jacob Walser <jwalser90@gmail.com>
2019-08-30 00:41:54 -0400

    usart: add read, line break, and autobaud

--- src/io/usart.c
+++ src/io/usart.c
@@ -1,6 +1,26 @@
 #include "usart.h"
 
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/vector.h>
 #include <libopencm3/stm32/usart.h>
+
+void usart1_isr()
+{
+    usartISR(USART1);
+}
+
+void usart2_isr()
+{
+    usartISR(USART2);
+}
+
+void usartISR(uint32_t usart)
+{
+    if (USART_ISR(usart) & USART_ISR_LBDF) {
+        usartLineBreakISR(usart);
+        USART_ICR(usart) |= USART_ICR_LBDCF;
+    }
+}
 
 void usartInit(uint32_t usart, uint32_t baudrate)
 {
@@ -18,4 +38,40 @@ void usartWrite(uint32_t usart, const char* string)
         c = string[i++];
         usart_send_blocking(usart, c);
     } while(c != 0);
+}
+
+void usartRead(uint32_t usart, char* byte, uint16_t length) {
+
+    while(length--) {
+      *byte++ = usart_recv_blocking(usart);
+    }
+}
+
+void usartStartAutoBaud(uint32_t usart) {
+    USART_CR2(usart) |= USART_CR2_ABREN;
+
+    // this next write is necessary to trigger subsequent autobaudrate sequences
+    // it is not strictly necessary for the first autodetection sequence,
+    // but it doesn't hurt
+
+    // aaayyy wtf the datasheet autobaudrate functional description says do this
+    // USART_ISR(usart) &= ~USART_ISR_ABRF;
+
+    // the register description for USART_ISR_ARBF says do this
+    USART_RQR(usart) |= USART_RQR_ABKRQ;
+}
+
+// note the usart must support lin mode
+// these bits can only be written when usart is disabled (UE = 0)
+void usartEnableLineBreakDetection(uint32_t usart)
+{
+    USART_CR2(usart) |= USART_CR2_ABRMOD_FRAME_0x55 | USART_CR2_LBDL | USART_CR2_LINEN;
+}
+
+void usartLineBreakISREnable(uint32_t usart) {
+    USART_CR2(usart) |= USART_CR2_LBDIE;
+}
+
+void usartLineBreakISRDisable(uint32_t usart) {
+    USART_CR2(usart) &= ~USART_CR2_LBDIE;
 }
--- src/io/usart.h
+++ src/io/usart.h
@@ -3,4 +3,15 @@
 #include <inttypes.h>
 
 void usartInit(uint32_t usart, uint32_t baudrate);
+
 void usartWrite(uint32_t usart, const char* string);
+void usartRead(uint32_t usart, char* byte, uint16_t length);
+
+void usartStartAutoBaud(uint32_t usart);
+
+void usartEnableLineBreakDetection(uint32_t usart);
+void usartLineBreakISREnable(uint32_t usart);
+void usartLineBreakISRDisable(uint32_t usart);
+
+void usartISR(uint32_t usart);
+void usartLineBreakISR(uint32_t usart);
