6b116fa2266a07d4a3c45855fe915f46e11a212f
Jacob Walser <jwalser90@gmail.com>
2019-08-30 00:44:45 -0400

    target: fill out usart definitions

--- src/target/nucleo-f042/target.h
+++ src/target/nucleo-f042/target.h
@@ -1,11 +1,17 @@
 #pragma once
 
-#include <libopencm3/stm32/usart.h>
+#define USART_PERIPH USART1
+#define USART_PERIPH_RCC RCC_USART1
+#define USART_IRQ NVIC_USART1_IRQ
 
-#define USART_PERIPH USART2
-#define USART_PERIPH_RCC RCC_USART2
+#define USART_GPIO_AF GPIO_AF1
 
+// built-in stlink VCP is on USART2 PA2 (TX) and PA15 (RX)
+// but it does not have LIN support needed for break detection
 #define USART_TX_GPIO_RCC RCC_GPIOA
 #define USART_TX_GPIO GPIOA
-#define USART_TX_GPIO_PIN GPIO2
-#define USART_TX_GPIO_AF GPIO_AF1
+#define USART_TX_GPIO_PIN GPIO9
+
+#define USART_RX_GPIO_RCC RCC_GPIOA
+#define USART_RX_GPIO GPIOA
+#define USART_RX_GPIO_PIN GPIO10
